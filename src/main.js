9eb6ec043eadb1b718859ad93b0bb4c6cc1440e9
jack <admin@jack.com>
2021-11-18 10:10:09 +0800

    header

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css' // 解决1像素边框
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body) // 解决click延迟3秒
 
 /* eslint-disable no-new */
 new Vue({
