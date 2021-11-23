ebe4b9b6d4718718450f7a12364a81f3bf493331
jack <admin@jack.com>
2021-11-23 15:38:26 +0800

    add search to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,36 +1,22 @@
 <template>
-    <div class="header">
-        <router-link to="/">
-            <span class="iconfont header-back">&#xe624;</span>
-        </router-link>
-        城市选择
+    <div>
+        <city-header></city-header>
+        <city-search></city-search>
     </div>
-
 </template>
 
 <script>
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
-  name: 'CityHeader'
+  name: 'City',
+  components: {
+    CityHeader,
+    CitySearch
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-    @import '~styles/varibles.styl'
-    .header
-        position relative
-        overflow: hidden
-        height: $headerHeight
-        line-height: $headerHeight
-        text-align: center
-        color: #fff
-        background: $bgColor
-        font-size: .32rem
-    .header-back
-        position: absolute
-        top: 0
-        left: 0
-        width: .64rem
-        text-align: center
-        font-size: .4rem
-        color: #fff
+
 </style>
