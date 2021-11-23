1e44e0f46720cc64f9d01c70e385023a73a49574
jack <admin@jack.com>
2021-11-23 13:23:23 +0800

    city router

--- src/pages/home/components/header.vue
+++ src/pages/home/components/header.vue
@@ -6,9 +6,11 @@
         <div class="header-input">
             <span class="iconfont">&#xe632;</span>
             输入城市/景点/游玩主题</div>
-        <div class="header-right">{{this.city}}
-             <span class="iconfont arrow-icon">&#xe64a;</span>
-             </div>
+        <router-link to="/city">
+            <div class="header-right">{{this.city}}
+                <span class="iconfont arrow-icon">&#xe64a;</span>
+            </div>
+        </router-link>
     </div>
 </template>
 
@@ -24,7 +26,7 @@ export default {
     @import '~styles/varibles.styl'
     .header
         display: flex
-        height: .86rem
+        height: $headerHeight
         background: $bgColor
         color: #fff
         .header-left
@@ -49,6 +51,7 @@ export default {
             margin-top: .32rem
             float: right
             text-align: center
+            color: #fff
             .arrow-icon
                 margin-left: -.04rem
                 font-size: .24rem
