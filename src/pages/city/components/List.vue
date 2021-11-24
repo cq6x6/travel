8c77e6d7ad2a16a9e62ef7b3f6e6d22d7a88c758
jack <admin@jack.com>
2021-11-24 21:01:31 +0800

    perfomance

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,14 +17,17 @@
                 热门城市
           </div>
           <div class="button-list">
-            <div class="button-wrapper" v-for="item of hot" :key="item.id">
+            <div class="button-wrapper"
+            v-for="item of hot" :key="item.id">
                         <div class="button">
                             {{item.name}}
                         </div>
                     </div>
             </div>
       </div>
-      <div class="area" v-for="(item, key) of cities" :key="key">
+      <div class="area" v-for="(item, key) of cities"
+      :key="key"
+      :ref="key">
           <div class="title border-topbottom">
                 {{key}}
           </div>
@@ -39,14 +42,22 @@
       </div>
   </div>
 </template>
-
 <script>
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
