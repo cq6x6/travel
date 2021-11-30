fa52f788da0645ee6a0de73bc4f65c441b98db6c
jack <admin@jack.com>
2021-11-30 16:47:44 +0800

    banner finish

---
+++ src/router/dist/index.dev.js
@@ -0,0 +1,38 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _vue = _interopRequireDefault(require("vue"));
+
+var _vueRouter = _interopRequireDefault(require("vue-router"));
+
+var _Home = _interopRequireDefault(require("@/pages/home/Home"));
+
+var _City = _interopRequireDefault(require("@/pages/city/City"));
+
+var _Detail = _interopRequireDefault(require("@/pages/detail/Detail"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_vue["default"].use(_vueRouter["default"]);
+
+var _default = new _vueRouter["default"]({
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: _Home["default"]
+  }, {
+    path: '/city',
+    name: 'City',
+    component: _City["default"]
+  }, {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: _Detail["default"]
+  }]
+});
+
+exports["default"] = _default;
